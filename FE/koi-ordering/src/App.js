a873c1bb156bef46773e0dc15bc78a72e032b656
Ngoc Thong <thonglyngocse@gmail.com>
2024-11-28 12:28:09 +0700

    code don gian booking order

--- FE/koi-ordering/src/App.js
+++ FE/koi-ordering/src/App.js
@@ -4,20 +4,20 @@ import Header from './components/Head/Header';
 import Footer from './components/Foot/Footer';
 import Login from './assets/user/login/Login';
 import Register from './assets/user/register/Register';
-import HomePage from './assets/user/home/HomePage'; // Import HomePage
+import HomePage from './assets/user/home/HomePage';
+import BookingOrder from './assets/user/bookingorder/BookingOrder';
 
 function App() {
   return (
     <div>
       <Header />
       <Routes>
-        {/* Đường dẫn mặc định Login */}
+        {/* Đường dẫn mặc định HomePage */}
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        
-        {/* Trang HomePage sau khi login */}
         <Route path="/home" element={<HomePage />} />
+        <Route path="/bookingorder" element={<BookingOrder />} />
       </Routes>
       <Footer />
     </div>
